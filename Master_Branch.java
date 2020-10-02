9b62323a44f9fd40fac4bdb6008cb400eafaaf3d
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-02 15:22:41 +0530

    Master_Branch.java

--- Master_Branch.java
+++ Master_Branch.java
@@ -1,11 +1,73 @@
 package objectOriented;
 
-public class Master_Branch {
+import java.util.*;
 
-	public static void main(String[] args){
-		System.out.println("Welcome to Address Book Program in AdressBookMain class on Master Branch");
-		
+
+
+public class EditContact {
+	private int numOfContact = 0;
+	private ContactDetails[] contactArray;
+	private EditContact() {
+		contactArray = new ContactDetails[5];
+	}
+	
+	private void addContactDetails(String firstName, String lastName, String address, String state, int zip, long phoneNo, String emailId) {
+		contactArray[numOfContact] = new ContactDetails(emailId, emailId, emailId, emailId, zip, phoneNo, emailId);
+		contactArray[numOfContact].setContactDetails(firstName, lastName, address, state, zip, phoneNo, emailId);
+		numOfContact++;
+	}
+	
+	private void editContactDetails(String firstName, String lastName, String address, String state, int zip, long phoneNo, String emailId) {
+		for(int i = 0; i < numOfContact; i++) {
+			if(contactArray[i].getFirstName().equals(firstName) && contactArray[i].getLastName().equals(lastName)) {
+				contactArray[i].setContactDetails(firstName, lastName, address, state, zip, phoneNo, emailId);
+				System.out.println("Address changed");
+				System.out.println(contactArray[i]);
+				break;
+			}
+		}
 	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		EditContact  addressBook = new EditContact ();
+		System.out.println("No. of contact details to enter (upto 5): ");
+		int numOfContact = sc.nextInt();
+		sc.nextLine();
+		for(int i = 0; i < numOfContact; i++) {
+			System.out.println("First Name: ");
+			String firstName = sc.nextLine();
+			System.out.println("Last Name: ");
+			String lastName = sc.nextLine();
+			System.out.println("Address: ");
+			String address = sc.nextLine();
+			System.out.println("State: ");
+			String state = sc.nextLine();
+			System.out.println("ZIP: " );
+			int zip = sc.nextInt();
+			System.out.println("Phone No: ");
+			long phoneNo = sc.nextLong();
+			sc.nextLine();
+			System.out.println("Email ID: ");
+			String emailId = sc.nextLine();
+			addressBook.addContactDetails(firstName, lastName, address, state, zip, phoneNo, emailId);
+		}
+		System.out.println("Enter details for contact record you want to edit");
+			System.out.println("First Name: ");
+			String firstName = sc.nextLine();
+			System.out.println("Last Name: ");
+			String lastName = sc.nextLine();
+			System.out.println("Address: ");
+			String address = sc.nextLine();
+			System.out.println("State: ");
+			String state = sc.nextLine();
+			System.out.println("ZIP: " );
+			int zip = sc.nextInt();
+			System.out.println("Phone No: ");
+			long phoneNo = sc.nextLong();
+			sc.nextLine();
+			System.out.println("Email ID: ");
+			String emailId = sc.nextLine();
+			addressBook.editContactDetails(firstName, lastName, address, state, zip, phoneNo, emailId);
+	}
 }
-
