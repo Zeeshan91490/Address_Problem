5dd0a64b32fd7901cd468913f4ebb8ed02ebc492
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-02 15:15:42 +0530

    Master_Branch.java

--- Master_Branch.java
+++ Master_Branch.java
@@ -1,11 +1,35 @@
 package objectOriented;
 
-public class Master_Branch {
+public class Address {
 
-	public static void main(String[] args){
-		System.out.println("Welcome to Address Book Program in AdressBookMain class on Master Branch");
-		
+	
+	private String firstName;
+	private String lastName;
+	private String address; 
+	private String state;
+	private int zip;
+	private long phoneNo;
+	private String emailId;
+	
+	public Address(String firstName, String lastName, String address, String state, int zip, long phoneNo, String emailId) { 
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.emailId = emailId;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "Name: " + firstName + " " + lastName + " Address: " + address +"State:"+ state +"zip:"+ zip+",Phone no:"+ phoneNo +",Email id:"+ emailId;
+	}
+	
+	public static void main(String[] args) {
+	Address contact = new Address("Zeeshan", "Khan,", "EPIP, Bengaluru,", "Karnataka,", 123456, 123456789 , "zeeshankhan@gmail.com");
+		System.out.println(contact);
+	}
 }
+
 
