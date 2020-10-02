0e66771fa9d65c6fdfe33408812b1502fcea055d
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-02 15:10:08 +0530

    Master_Branch.java

---
+++ Master_Branch.java
@@ -0,0 +1,11 @@
+package objectOriented;
+
+public class Master_Branch {
+
+	public static void main(String[] args){
+		System.out.println("Welcome to Address Book Program in AdressBookMain class on Master Branch");
+		
+	}
+
+}
+
